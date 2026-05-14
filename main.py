cf2bc88c264ad67f975ddade71267f89d3caeb0c
Kazesouth <18776907423@163.com>
2026-05-14 22:00:21 +0800

    second commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from gui import FarmGUI
 
 if __name__ == "__main__":
     # 配置底部的标识，便于在Windows任务栏统一图标
-    system_ops.set_app_model_id("ChenFeng.NikkiFarmhelper.v1.48")
+    system_ops.set_app_model_id("ChenFeng.NikkiFarmhelper.v1.8")
     
     # 强制让程序感知高清DPI屏幕缩放
     system_ops.set_dpi_awareness()
